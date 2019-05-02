b20f75ebdd0887abc27cd132cfa925d1cd8605be
Daniel Kang <mingeek@umich.edu>
2019-05-02 01:31:06 -0400

    all convs instead of just one

--- analyze.py
+++ analyze.py
@@ -1,17 +1,27 @@
 from sentiment import *
 import matplotlib.pyplot as plt
 import pandas as pd
+import itertools
 pd.plotting.register_matplotlib_converters()
 
 
-fb_to_json()
+fb_to_json() #don't run this every time
 chats = get_chats()
-chat = chat_sentiment_analysis(get_json(chats[0])) #insert names here
-df = chat_sentiment_analysis_time(chat)
+
+
+
+#all conversations
+convs = [chat_sentiment_analysis(get_json(chat)) for chat in chats]
+all_convs = list(itertools.chain.from_iterable(convs))
+
+df = chat_sentiment_analysis_time(all_convs)
 date = df.index.tolist()
 sentiment = df['sentiment'].tolist()
 
-plt.plot(date, sentiment)
-plt.ylabel('Sentiment')
+f, ax = plt.subplots()
+ax.plot(date, sentiment, 'b')
+ax.set_title('Relationship Comparison')
+ax.set_ylabel('Sentiment')
 plt.gcf().autofmt_xdate()
+plt.grid(True, which='both')
 plt.show()
