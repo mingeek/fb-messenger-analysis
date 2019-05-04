ce5cd7a15e766911ef91fe1719e7d9103a76c950
Daniel Kang <mingeek@umich.edu>
2019-05-04 18:56:13 -0400

    New Plot functionality

--- plot.py
+++ plot.py
@@ -9,13 +9,40 @@ def plot_one(x, y, title, ylabel):
     plt.grid(True, which='both')
     plt.show()
 
-def plot_two(onex, oney, twox, twoy, title='Relationship Comparison'):
+#Input: Graphs, which is a list of objects
+def plot_multi(graphs, title='Relationship Comparison', ylabel='Sentiment'):
     f, ax = plt.subplots()
-    ax.plot(onex, oney, 'C0', label='me')
-    ax.plot(twox, twoy, 'C1', label='them')
+    for i in range(len(graphs)): 
+        ax.plot(graphs[i]['x'], graphs[i]['y'], 'C'+ str(i), label=graphs[i]['label'])
     ax.set_title(title)
-    ax.set_ylabel('Sentiment')
+    ax.set_ylabel(ylabel)
     plt.gcf().autofmt_xdate()
     plt.grid(True, which='both')
-    ax.legend(loc=2)
+    plt.figlegend(loc=2)
     plt.show()
+
+#Input: Graphset, which is a list of lists (size 2), with each inner list holding a JSON used to plot the figure
+def plot_multi_two(graphset, title='Relationship Comparison', ylabel='sentiment'):
+    f, ax = plt.subplots()
+    if(len(graphset) == 2):
+        axes = [
+            ax,
+            ax.twinx() #this lets you print the other axis on the opposite side
+        ]
+    else:
+        return
+    color_counter = 0
+    for n in range(len(graphset)):
+        axes[n].set_ylabel(graphset[n][0]['ylabel'])
+        #graphs will have its own info on ylabel
+        for i in range(len(graphset[n])): 
+            axes[n].plot(graphset[n][i]['x'], graphset[n][i]['y'], 'C'+ str(color_counter), label=graphset[n][i]['label'])
+            color_counter += 1
+    handles, labels = ax.get_legend_handles_labels()
+    f.legend(handles, labels, loc=2)
+    ax.set_title(title)
+    plt.gcf().autofmt_xdate()
+    plt.grid(True, which='both')
+    plt.figlegend(loc=2)
+    plt.show()
+    
